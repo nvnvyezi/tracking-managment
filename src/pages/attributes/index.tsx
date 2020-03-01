de4fe42791fdd71bfa6345a0625dc786d2b042be
liliye <nvnvyezi@gmail.com>
2020-03-01 22:40:17 +0800

    fix(pages): content layout

--- src/pages/attributes/index.tsx
+++ src/pages/attributes/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import Content from '@/layout/content'
+
 import './index.scss'
 
-export default function AddPoint() {
-  return <div className="attributes-title">attributes</div>
+export default function Attributes() {
+  return (
+    <Content
+      crumbData={[
+        { label: '/home/welcome', value: '首页' },
+        { label: '', value: '属性管理' },
+      ]}
+    >
+      <div>event</div>
+    </Content>
+  )
 }
--- src/pages/create-track/index.tsx
+++ src/pages/create-track/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export default function AddPoint() {
-  return <div>add point</div>
+import Content from '@/layout/content'
+
+export default function createPoint() {
+  return (
+    <Content
+      crumbData={[
+        { label: '/home/welcome', value: '首页' },
+        { label: '', value: '新增埋点' },
+      ]}
+    >
+      <div>event</div>
+    </Content>
+  )
 }
